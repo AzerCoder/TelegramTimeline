6c9e7a362141fb99ce78501768b4274eea87bc66
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2023-12-26 18:24:54 +0500

    Initial Commit

---
+++ TelegramTimeline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TelegramTimeline
+//
+//  Created by A'zamjon Abdumuxtorov on 26/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TelegramTimeline/TelegramTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramTimelineApp.swift
+//  TelegramTimeline
+//
+//  Created by A'zamjon Abdumuxtorov on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TelegramTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
